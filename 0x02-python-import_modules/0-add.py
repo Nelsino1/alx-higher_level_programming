735fed7073bbb92e2f4b77779bfbd57f972bacef
Samuel Edet Asukwo <samuelnelsino@gmail.com>
2023-12-11 03:36:59 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+!/usr/bin/python3
+a = 1
+b = 2
+add_0 = __import__('add_0')
+print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
 
-    a = 1
-    b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
